0e69a549173ebd420e560b26a24af53d1ec64820
Karisar <kari.sarsila@gmail.com>
2015-10-21 19:03:07 +0300

    uusi controlleri

---
+++ src/com/sarsila/surveillance/controller/ParamTestController.java
@@ -0,0 +1,21 @@
+package com.sarsila.surveillance.controller;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+
+public class ParamTestController implements Controller {
+	 protected final Log logger = LogFactory.getLog(getClass());
+	 public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			    throws ServletException, IOException 
+	  		{
+			    logger.info("Return View");
+			    System.out.println("meni");
+			    return new ModelAndView("paramtest.jsp");
+			  }
+}
